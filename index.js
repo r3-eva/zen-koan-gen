60ba306c58836b1e5d3ccade553259cd296825b8
r3-eva <riley@r3evaluate.com>
2023-07-26 11:35:12 -0600

    added 3 data set arrays to index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// Zen Koans example dictionaries
+
+const zenFirst = ['not engaging in ignorance is wisdom', "don't waste your time chasing butterflies", 'muddy water is best cleared by leaving it alone', 'the less you need', 'you must let what happens happen', 'dwell not in the inner void', 'there is only one reality', 'un-know nothingness is in fact', 'become comfortable with not knowing'];
+const zenMiddle = ['that is our practice', 'the whole world belongs to you', 'listen', 'you become the universe', 'keep climbing', 'grasp nothing', 'every moment is the guru', 'the reservoir of infinite possiblities', 'is the most important thing'];
+const zenLast = ['Qigang Nakayama', 'Hirokumi Kojima', 'Iwao Murata', 'Masashi Sumisu', 'Jotaro Zhan', 'AKio Okita', 'Kamatari Sakata', 'Bunrakuken Kanbayashi', 'Shinichi Okimoto', 'Sekien Miyashita'];
+
