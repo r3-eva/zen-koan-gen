95b781a489579574e947a9320345882f4f81f10c
r3-eva <riley@r3evaluate.com>
2023-07-26 11:56:43 -0600

    added random return function to pick a random phrase from each correponding array

--- index.js
+++ index.js
@@ -2,5 +2,23 @@
 
 const zenFirst = ['not engaging in ignorance is wisdom', "don't waste your time chasing butterflies", 'muddy water is best cleared by leaving it alone', 'the less you need', 'you must let what happens happen', 'dwell not in the inner void', 'there is only one reality', 'un-know nothingness is in fact', 'become comfortable with not knowing'];
 const zenMiddle = ['that is our practice', 'the whole world belongs to you', 'listen', 'you become the universe', 'keep climbing', 'grasp nothing', 'every moment is the guru', 'the reservoir of infinite possiblities', 'is the most important thing'];
-const zenLast = ['Qigang Nakayama', 'Hirokumi Kojima', 'Iwao Murata', 'Masashi Sumisu', 'Jotaro Zhan', 'AKio Okita', 'Kamatari Sakata', 'Bunrakuken Kanbayashi', 'Shinichi Okimoto', 'Sekien Miyashita'];
+const zenLast = ['Qigang Nakayama', 'Hirokumi Kojima', 'Iwao Murata', 'Masashi Sumisu', 'Jotaro Zhan', 'Kamatari Sakata', 'Bunrakuken Kanbayashi', 'Shinichi Okimoto', 'Sekien Miyashita'];
+
+// returns random zenFirst phrase
+const returnZenFirst = () => {
+    return zenFirst[Math.floor(Math.random() *9)];
+    console.log(returnZenFirst);
+};
+
+const returnZenMiddle = () => {
+    return zenMiddle[Math.floor(Math.random() *9)];
+    console.log(returnZenMiddle);
+};
+
+const returnZenLast = () => {
+    return zenLast[Math.floor(Math.random() *9)];
+    console.log(returnZenLast);
+};
+
+
 
